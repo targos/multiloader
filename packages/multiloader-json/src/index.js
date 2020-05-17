f2bedabe979ba5d220e606d78e9693baa895c45b
Michaël Zasso <targos@protonmail.com>
2020-05-17 11:37:18 +0200

    feat: add json loader

---
+++ packages/multiloader-json/src/index.js
@@ -0,0 +1,10 @@
+export default function jsonLoader() {
+  return {
+    getFormat(url, context, defaultGetFormat) {
+      if (url.endsWith('.json')) {
+        return { format: 'json' };
+      }
+      return defaultGetFormat(url, context, defaultGetFormat);
+    },
+  };
+}
