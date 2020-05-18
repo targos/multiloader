1b70fa3057921ed8e045d56c4045502f7695b6e5
Michaël Zasso <targos@protonmail.com>
2020-05-18 15:37:49 +0200

    add fs test and comment tests

--- test.mjs
+++ test.mjs
@@ -1,16 +1,19 @@
-import { Sha256 } from 'https://deno.land/std/hash/sha256.ts';
-const sha = new Sha256();
-sha.update('done');
-console.log(sha.hex());
+// import { Sha256 } from 'https://deno.land/std/hash/sha256.ts';
+// const sha = new Sha256();
+// sha.update('done');
+// console.log(sha.hex());
 
-import { decode, encode } from 'https://deno.land/std/encoding/utf8.ts';
-console.log(decode(encode('test')));
+// import { decode, encode } from 'https://deno.land/std/encoding/utf8.ts';
+// console.log(decode(encode('test')));
 
-import {
-  parse as parseToml,
-  stringify as stringifyToml,
-} from 'https://deno.land/std/encoding/toml.ts';
-console.log(stringifyToml(parseToml('test=1')));
+// import {
+//   parse as parseToml,
+//   stringify as stringifyToml,
+// } from 'https://deno.land/std/encoding/toml.ts';
+// console.log(stringifyToml(parseToml('test=1')));
 
-import { parse as parseCsv } from 'https://deno.land/std/encoding/csv.ts';
-parseCsv('test,a,b\n1,2,3\n4,5,6').then(console.log, console.error);
+// import { parse as parseCsv } from 'https://deno.land/std/encoding/csv.ts';
+// parseCsv('test,a,b\n1,2,3\n4,5,6').then(console.log, console.error);
+
+// import { readFileSync } from 'https://deno.land/std/node/fs.ts';
+// console.log(readFileSync('./nodeno/nodeno.js').toString());
