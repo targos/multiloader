20076fdb3a3068b36122e57a2b8d0d1cdf422cd6
Michaël Zasso <targos@protonmail.com>
2020-05-20 09:05:07 +0200

    more io and fs stuff

--- nodeno/nodeno.js
+++ nodeno/nodeno.js
@@ -14,6 +14,7 @@ window.Deno = {
   ...require('./nodeno.permissions.js'),
   test: require('./nodeno.test'),
   ...require('./nodeno.net.js'),
+  ...require('./nodeno.io.js'),
 };
 
 window.crypto = require('./crypto.js');
