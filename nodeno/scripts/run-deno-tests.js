2b471db459b5a738559da66afd01c74dbd5386cf
Michaël Zasso <targos@protonmail.com>
2020-05-23 16:04:31 +0200

    fix run-deno-tests and allow to filter tests

--- nodeno/scripts/run-deno-tests.js
+++ nodeno/scripts/run-deno-tests.js
@@ -1,38 +1,62 @@
 'use strict';
 const cp = require('child_process');
 const path = require('path');
+const { pathToFileURL } = require('url');
 
 const rPath = path.join(__dirname, '..', 'nodeno.js');
-const loaderPath = path.join(__dirname, '..', '..', 'test-loader.js');
-const testScriptPath = path.join(__dirname, '..', '..', 'nodeno-tests', 'test.js');
+const loaderPath = pathToFileURL(
+  path.join(__dirname, '..', '..', 'test-loader.js'),
+);
+const testScriptPath = path.join(
+  __dirname,
+  '..',
+  '..',
+  'nodeno-tests',
+  'test.js',
+);
 
 function runTests(testURL) {
-
-    process.stdout.write('TESTING ' + testURL + ' ');
-    const  { stdout, stderr, status } = cp.spawnSync('node', ['-r', rPath, '--loader', loaderPath, testScriptPath, testURL]);
-    if (status !== 0) {
-        console.log('\nFAILED', testURL);
-        console.log(stdout.toString());
-        console.log(stderr.toString());
-    }
-    else {
-        process.stdout.write('ok\n');
-    }
-    return status === 0;
+  process.stdout.write('TESTING ' + testURL + ' ');
+  const { stdout, stderr, status } = cp.spawnSync(process.execPath, [
+    '-r',
+    rPath,
+    '--loader',
+    loaderPath,
+    testScriptPath,
+    testURL,
+  ]);
+  if (status !== 0) {
+    console.log('\nFAILED', testURL);
+    console.log(stdout.toString());
+    console.log(stderr.toString());
+  } else {
+    process.stdout.write('ok\n');
+  }
+  return status === 0;
 }
 
-const targets = require('./test-urls.json');
+let targets = require('./test-urls.json');
+
+const args = process.argv.slice(2);
+if (args.length > 0) {
+  targets = targets.filter((target) =>
+    args.some((arg) => target.includes(arg)),
+  );
+}
+
 const failed = [];
 for (const url of targets) {
-    const res = runTests('https://deno.land/std/' + url);
-    if(!res) {
-        failed.push(url);
-    }
+  const res = runTests('https://deno.land/std/' + url);
+  if (!res) {
+    failed.push(url);
+  }
 }
 
 console.log('FAILED', failed.length, '/', targets.length);
-console.log('FAILED tests:\n', failed.map((x) => 'https://deno.land/std/' + x).join('\n'));
-if(failed.length > 0) {
-    process.exit(1);
+console.log(
+  'FAILED tests:\n',
+  failed.map((x) => 'https://deno.land/std/' + x).join('\n'),
+);
+if (failed.length > 0) {
+  process.exit(1);
 }
-
